d2530b772b116e6bdbaecb6311ca8cda37dbd734
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-10-06 00:41:33 +0300

    Added dnsmasq custom conf for more dns record types

--- pihole-sync.py
+++ pihole-sync.py
@@ -14,13 +14,18 @@ HA_SECONDARY_DOCKER_CONTAINER_NAME = 'pihole'
 
 def main():
     custom_list = get_custom_dnsmasq_list()
+    dnsmasq_custom_conf = get_custom_dnsmasq_conf()
 
     custom_list_ssh = send_ssh_command('cat {}'.format(
         os.path.join(HA_SECONDARY_PIHOLE_DIR, 'etc-pihole/custom.list')
     ))
 
-    if custom_list != custom_list_ssh:
-        print('Remote custom.list does not match, transferring new version...')
+    dnsmasq_custom_conf_ssh = send_ssh_command('cat {}'.format(
+        os.path.join(HA_SECONDARY_PIHOLE_DIR, 'etc-dnsmasq.d/02-custom.conf')
+    ))
+
+    if custom_list != custom_list_ssh or dnsmasq_custom_conf != dnsmasq_custom_conf_ssh:
+        print('Remote custom.list or 02-custom.conf does not match, transferring new version...')
 
         send_scp_command_local_to_remote(
             os.path.join(HA_MASTER_PIHOLE_DIR, 'etc-pihole/custom.list'),
@@ -30,6 +35,17 @@ def main():
         send_ssh_command('sudo mv {} {}'.format(
             os.path.join(HA_SECONDARY_PIHOLE_DIR, 'custom.list.temp'),
             os.path.join(HA_SECONDARY_PIHOLE_DIR, 'etc-pihole/custom.list')
+        ))
+
+        send_scp_command_local_to_remote(
+            os.path.join(HA_MASTER_PIHOLE_DIR, 'etc-dnsmasq.d/02-custom.conf'),
+            os.path.join(HA_SECONDARY_PIHOLE_DIR, '02-custom.conf.temp')
+        )
+
+        send_ssh_command('sudo mv {} {}'.format(
+            os.path.join(HA_SECONDARY_PIHOLE_DIR, '02-custom.conf.temp'),
+            os.path.join(HA_SECONDARY_PIHOLE_DIR,
+                         'etc-dnsmasq.d/02-custom.conf')
         ))
 
         try:
@@ -44,6 +60,18 @@ def main():
 
 def get_custom_dnsmasq_list():
     f = open(os.path.join(HA_MASTER_PIHOLE_DIR, 'etc-pihole/custom.list'), 'r')
+
+    lines = []
+
+    for line in f.readlines():
+        lines.append(line.strip())
+
+    return '\n'.join(lines)
+
+
+def get_custom_dnsmasq_conf():
+    f = open(os.path.join(HA_MASTER_PIHOLE_DIR,
+                          'etc-dnsmasq.d/02-custom.conf'), 'r')
 
     lines = []
 
